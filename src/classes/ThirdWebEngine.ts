eb221fa0380ec98d41f71c71c0583f08686b8ec9
easyrun42 <declaimyy@gmail.com>
2024-01-31 21:57:34 +0000

    decimal places 6

--- src/classes/ThirdWebEngine.ts
+++ src/classes/ThirdWebEngine.ts
@@ -10,7 +10,7 @@ export class ThirdWebEngine {
   public static isBalanceLow = async () => {
     const { result } = await httpFetchBalanceStatus();
     const formattedEther = ethers.utils.formatEther(result.value);
-    const totalFormattedBalance = BigNumber(formattedEther).dp(4).toNumber();
+    const totalFormattedBalance = BigNumber(formattedEther).dp(6).toNumber();
     // Make sue to include your own low balanec logic here. In this example
     // if polygon mainnet funds are lower than 0.01 it'll return true
     return totalFormattedBalance < 0.01;
